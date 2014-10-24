88158eaa637b1c303f71becc8b102a515940511d
Andrew Odewahn <odewahn@oreilly.com>
2014-10-24 13:45:29 -0400

    added commandline args to specify directories for input and output

--- pdc2md.go
+++ pdc2md.go
@@ -5,6 +5,9 @@ import (
    "io/ioutil"
    "encoding/json"
    "strings"
+   "flag"
+   "log"
+   "os"
 )
 
 
@@ -45,47 +48,90 @@ func md(n Notebook) string {
 }
 
 
+func convert(input_path, output_path string) {
+   base_dirs, _ := ioutil.ReadDir(input_path)
 
-
-func main() {
-	
-   BASE_PATH := "/Users/odewahn/Desktop/python-data-cookbook/notebooks"
-   OUTPUT_PATH := "/Users/odewahn/Desktop/pdc-generated"
-   base_dirs, _ := ioutil.ReadDir(BASE_PATH)
- 
    for _,d := range base_dirs {
 	  // pull all directories that have a "-" in the name
 	  dir_name := strings.Split(d.Name(),"-")
 	  if (d.IsDir() && len(dir_name) > 1) {
          chapter_fn := d.Name() + ".md"  // Root filename we're going to glob together
+         fmt.Printf("Creating file %s\n", chapter_fn)
          chapter_title := "# " + strings.Join(dir_name[1:len(dir_name)], " ") + "\n\n"
          chapter_contents := chapter_title        
-        
+
          // Process ipynb files in the directory and convert them into sections in the chapter
-         sections, _ := ioutil.ReadDir(BASE_PATH + "/" + d.Name())
+         sections, _ := ioutil.ReadDir(input_path + "/" + d.Name())
          for _,f := range sections {
 	        test_fn := strings.Split(f.Name(),".")
             if (test_fn[len(test_fn)-1] == "ipynb") {
-	           section_fn := BASE_PATH + "/" + d.Name() + "/" + f.Name()
-	
+	           section_fn := input_path + "/" + d.Name() + "/" + f.Name()
+
 			   // Grab the contents of the section
 			   ipynb, err := ioutil.ReadFile(section_fn)
 			   if err != nil {
 			      panic(err)
 			   }
-			
+
 			   // unmarshall the json data into a notebook structure
 			   var notebook Notebook
 			   err = json.Unmarshal(ipynb, &notebook)
-			
+
 			   // append the contents to the chapter output
 	           chapter_contents = chapter_contents + md(notebook) + "\n"      
             }
          }
-         err := ioutil.WriteFile(OUTPUT_PATH + "/" + chapter_fn, []byte(chapter_contents), 0644)
+         err := ioutil.WriteFile(output_path+ "/" + chapter_fn, []byte(chapter_contents), 0644)
 	     if err != nil {
 	        panic(err)
 	     }
       }
    }
+	
+}
+
+
+// Confirm that a given string represents an existing directory
+func check_dir(dir string) string {
+	
+	inDir, err := os.Stat(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Make sure the output directory exists and is a direcotry, not a file
+	if !inDir.IsDir() {
+		log.Fatal("%s is not a directory", dir)
+	}
+	return dir
+}
+
+
+
+func main() {
+	
+	
+	
+	var inPtr = flag.String("in", "./notebooks", "the input directory; this should be to top level directory of the repo")
+	var outPtr = flag.String("out", "./markdown", "the output directory; by default, it's ./markdown, but you can override that")
+	
+	flag.Parse()
+	
+	
+	// Make sure the input directory exists and is a direcotry, not a file
+	inDir := check_dir(*inPtr)
+	outDir := check_dir(*outPtr)
+	
+	fmt.Printf("Converting files in %s and exporting them to %s\n", inDir, outDir)
+	
+    convert(inDir, outDir)
+	
+
+
+	
+	
+//   BASE_PATH := "/Users/odewahn/Desktop/python-data-cookbook/notebooks"
+//   OUTPUT_PATH := "/Users/odewahn/Desktop/pdc-generated"
+
+
 }
